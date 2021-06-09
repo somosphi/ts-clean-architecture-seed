fc87e0506a1937af793df12c250d1d880738c6fd
ciro.azevedo <ciro.azevedo@4all.com>
2021-06-09 16:43:33 -0300

    add decoretor queue and validationSchema for message amqp

--- src/presentation/amqp/consumers/findUserById/find-user-by-id.ts
+++ src/presentation/amqp/consumers/findUserById/find-user-by-id.ts
@@ -1,35 +1,35 @@
-import { inject } from 'tsyringe';
-import { ConsumeMessage } from 'amqplib';
+import { inject, injectable } from 'tsyringe';
+import { Channel, ConsumeMessage } from 'amqplib';
 import { Consumer } from '@/presentation/amqp/consumers/consumer';
 import { IListUsersByIdUseCase } from '@/core/useCases/listUsersById/list-users-by-id.interface';
 import { FindUserMessage } from '@/presentation/amqp/consumers/findUserById/find-user-by-id.dto';
-import { validation } from '@/presentation/amqp/middlewares/validation';
 import { findUserSchema } from '@/presentation/amqp/consumers/findUserById/find-user-by-id.schema';
 import { User } from '@/core/entities/user';
 import { logger } from '@/logger';
-import { convertToJson } from '@/shared/helper/buffer-converter';
+import { queue, validationSchema } from '../consume.config';
 
+@injectable()
+@queue('user.find')
 export class FindUserByIdConsumer extends Consumer {
   constructor(
     @inject('ListUsersByIdUseCase')
     private listUsersByIdUseCase: IListUsersByIdUseCase
   ) {
-    super('user.find');
+    super();
   }
 
-  messageHandler(message: ConsumeMessage | null): void {
-    if (message) {
-      const messageContent: FindUserMessage = validation(findUserSchema)<
-        FindUserMessage
-      >(convertToJson(message.content));
-      this.findUserById(messageContent);
-    }
-  }
-
-  protected async findUserById(findUserMessage: FindUserMessage) {
-    const user: User = await this.listUsersByIdUseCase.listById(
-      findUserMessage.id
-    );
+  @validationSchema(findUserSchema)
+  async messageHandler(message: FindUserMessage): Promise<void> {
+    const user: User = await this.listUsersByIdUseCase.listById(message.id);
+    
     logger.info(JSON.stringify(user));
+  }
+
+  onConsumeError(
+    err: Error,
+    channel: Channel,
+    message: ConsumeMessage | null
+  ): void {
+    logger.error(err);
   }
 }
